fa90f6d5ced2adb33345a7f17e86ebcf3e267a56
amasotti <toniomasotti@gmail.com>
2024-01-14 13:05:40 +0100

    docs: update pydocs

--- bot/wikivoyage/VoyBot.py
+++ bot/wikivoyage/VoyBot.py
@@ -11,15 +11,36 @@ class WikivoyageBot(WikiBot):
         self.current_page = None
 
     def set_current_page(self, page):
+        """
+        Set the current page, to be then used in logging
+        :param page:
+        :return:
+        """
         self.current_page = page
 
     def parse_listed_destinations(self, wikitext):
+        """
+        Parse the destinationlist template in the given wikitext
+        :param wikitext:
+        :return:
+        """
         return self.parse_nested_template(wikitext, DESTINATION_TEMPLATE_ITEM_NAME)
 
     def parse_listed_cities(self, wikitext):
+        """
+        Parse the citylist template in the given wikitext
+        :param wikitext:
+        :return:
+        """
         return self.parse_nested_template(wikitext, CITY_TEMPLATE_ITEM_NAME)
 
     def process_wikidata_in_citylist(self, templates):
+        """
+        Add wikidata ids to the citylist templates if they are missing and can be found, updating the
+        templates in place
+        :param templates:
+        :return:
+        """
         wd_bot = WikidataBot()
         for template in templates:
             # Conditions
@@ -35,6 +56,14 @@ class WikivoyageBot(WikiBot):
         return templates
 
     def _process_wikidata(self, name, wikidata_id, template):
+        """
+        Add the wikidata id to the template if it exists. Basically a check that the given wikidata id
+        is not an empty string. Also logs the operation
+        :param name:
+        :param wikidata_id:
+        :param template:
+        :return:
+        """
         if wikidata_id == "":
             pywikibot.logging.stdout(f"\tCould not find wikidata item for {name} -- keeping empty")
             self.write_log_line(f"{self.current_page} -- No wikidata item found for {name}")
@@ -43,5 +72,11 @@ class WikivoyageBot(WikiBot):
             template.add("wikidata", wikidata_id)
 
     def write_log_line(self, text, file="logs/citylist_log.log"):
+        """
+        Write a line to the log file
+        :param text: the log line
+        :param file: the log file
+        :return: None
+        """
         with open(file, "a") as f:
             f.write(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')} {text}\n")
