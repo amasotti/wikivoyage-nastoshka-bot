b7fd1d8d9f12faa42874720cd7df550141d22642
amasotti <toniomasotti@gmail.com>
2024-01-16 17:05:08 +0100

    chore: clean up and modularize

--- bot/wikivoyage/scripts/categorize_region_without_destinations.py
+++ bot/wikivoyage/scripts/categorize_region_without_destinations.py
@@ -2,54 +2,61 @@ import pywikibot.logging
 
 from bot.wikivoyage import WikivoyageBot
 
+LOG_FILE_PATH = "logs/region_without_destinations.log"
+
 
 def categorize_region_without_destinations(target="Stato"):
     bot = WikivoyageBot(lang="it")
+    articles = bot.listify_category(target, 0)
 
-    citylist = bot.get_template("Citylist")
-    destinationlist = bot.get_template("Destinationlist")
-
-    # 1. Retrieve all regions and states articles
-    articles = bot.listify_category(target,0)
-    total_articles = len(articles)
-
-    # 2. Check if there is a Citylist or Destinationlist template in the article (exclude comments)
+    # Initialize a list of articles to be categorized
     to_be_categorized = []
-    i = 0
-    for article in articles:
-        i += 1
-        print(f"Processing article {i} of {total_articles}")
 
-        bot.set_current_page(article.title())
-
-        has_citylist = False
-        has_destinationlist = False
-
-        templates = bot.get_page_templates(bot.current_page)
-
-        for template in templates:
-            template_page, params = template
-            if template_page == citylist:
-                has_citylist = True
-                break
-            if template_page == destinationlist:
-                has_destinationlist = True
-                break
+    for i, article in enumerate(articles, start=1):
+        print(f"Processing article {i} of {len(articles)}")
+        has_citylist, has_destinationlist = _check_relevant_templates(bot, article)
 
         if not has_citylist and not has_destinationlist:
             pywikibot.logging.info(f"Article {article.title()} has no Citylist or Destinationlist template")
             to_be_categorized.append(article.title())
-            if bot.is_in_category(article.title(), "Categoria:Regioni senza Citylist o Destinationlist"):
-                continue
-            else:
+            if not bot.is_in_category(article.title(), "Categoria:Regioni senza Citylist o Destinationlist"):
                 bot.add_category(bot.current_page, "Regioni senza Citylist o Destinationlist")
-        bot.set_current_page(None)
-    # 3. Dump a list of these articles to a file
-    bot.write_log_line("Articles without Citylist or Destinationlist templates:", "logs/region_without_destinations.log")
-    for article in to_be_categorized:
-        bot.write_log_line(
-            f"* [[{article}]]",
-            "logs/region_without_destinations.log", with_timestamp=False)
 
-    # 4. Categorize the article in Category:Regions without destinations
+    # Dump a list of these articles to a file
+    bot.write_log_line(f"Articles of type {target} without Citylist or Destinationlist templates:", LOG_FILE_PATH)
+    bot.write_log_line(
+        '\n'.join([f"* [[{article}]]" for article in to_be_categorized]),
+        LOG_FILE_PATH, with_timestamp=False)
 
+
+def _check_relevant_templates(bot, article):
+    """
+    Check if the specified article has specific templates.
+
+    :param bot: The instance of the bot.
+    :type bot: Bot
+    :param article: The title of the article to check.
+    :type article: str
+    :return: A tuple indicating whether the article has Citylist template and Destinationlist template respectively.
+    :rtype: tuple
+    """
+    bot.set_current_page(article.title())
+
+    citylist = bot.get_template("Citylist")
+    destinationlist = bot.get_template("Destinationlist")
+
+    has_citylist = False
+    has_destinationlist = False
+
+    templates = bot.get_page_templates(bot.current_page)
+
+    for template in templates:
+        template_page, params = template
+        if template_page == citylist:
+            has_citylist = True
+            break
+        if template_page == destinationlist:
+            has_destinationlist = True
+            break
+    bot.set_current_page(None)
+    return has_citylist, has_destinationlist
