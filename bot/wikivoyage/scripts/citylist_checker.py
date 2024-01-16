97984353b1ba1224e116d91579ca8c17616d60eb
amasotti <toniomasotti@gmail.com>
2024-01-16 18:54:44 +0100

    chore: rm debug, simplify naming

--- bot/wikivoyage/scripts/citylist_checker.py
+++ bot/wikivoyage/scripts/citylist_checker.py
@@ -11,28 +11,28 @@ def citylist_wikidata_check(lang="it", total=1):
     for the same city name are not allowed)
     :return: None
     """
-    wikivoyage_bot = WikivoyageBot(lang=lang)
+    voy_bot = WikivoyageBot(lang=lang)
 
     # Example usage of listing a category
-    articles = wikivoyage_bot.listify_category("Itemlist con errori di compilazione", total)
+    articles = voy_bot.listify_category("Itemlist con errori di compilazione", total)
 
     # Read the text of the article
     for article in articles:
-        wikivoyage_bot.set_current_page("Sassonia-Anhalt")
-        pywikibot.logging.stdout(f"Checking article: {wikivoyage_bot.current_page}")
+        voy_bot.set_current_page(voy_bot.current_page)
+        pywikibot.logging.stdout(f"Checking article: {voy_bot.current_page}")
 
         # Get and parse the page wikicode
-        updated_wikitext = wikivoyage_bot.get_page_text(wikivoyage_bot.current_page)
+        updated_wikitext = voy_bot.get_page_text(voy_bot.current_page)
         wikicode = mwparserfromhell.parse(updated_wikitext)
 
         # Extract the templates from the page
         templates = wikicode.filter_templates()
 
         # Add wikidata ids to the templates if they are missing and can be found
-        wikivoyage_bot.process_wikidata_in_citylist(templates)
+        voy_bot.process_wikidata_in_citylist(templates)
 
         # Save the page
-        page = wikivoyage_bot.get_page(wikivoyage_bot.current_page)
+        page = voy_bot.get_page(voy_bot.current_page)
         updated_wikitext = str(wikicode)
         page.text = updated_wikitext
         page.save(f"Aggiungo wikidata ids ai template citylist e destinationlist di {article.title()}",
@@ -40,9 +40,9 @@ def citylist_wikidata_check(lang="it", total=1):
                   minor=True,
                   )
         # Log the operation
-        wikivoyage_bot.write_log_line(
+        voy_bot.write_log_line(
             f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')} {article.title()} -- Added wikidata ids\n")
 
         # Reset the current page
-        wikivoyage_bot.set_current_page(None)
+        voy_bot.set_current_page(None)
     exit(0)
